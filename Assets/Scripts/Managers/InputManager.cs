e0a754b693102a179fc6a32c08d1ee028affacf9
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2016-11-17 17:48:20 -0500

    Added player movement!

---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour
+{
+    PlayerController playerController;           // refference to the playerController script
+
+    float xAxis = 0;                             // 1 = right, -1 = left
+    float zAxis = 0;                             // 1 = front, -1 back
+    float mouseXAxis = 0;                        // left or right movement of mouse (camera). Positive numb = right, Negative numb = left
+    float mouseYAxis = 0;                        // up or down movement of mouse (camera). Positive numb = up, Negative numb = down.
+    bool cameraLock = true;                      // constantly lock the cursor in the center
+
+    void Awake()
+    {
+        playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+    }
+
+    void FixedUpdate()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        mouseXAxis = Input.GetAxis("Mouse X");
+        mouseYAxis = Input.GetAxis("Mouse Y");
+
+        xAxis = Input.GetAxisRaw("Horizontal");
+        zAxis = Input.GetAxisRaw("Vertical");
+
+        if (mouseXAxis != 0 || mouseYAxis != 0)
+        {
+            playerController.Mouselook(mouseXAxis, mouseYAxis);
+        }
+
+        if (xAxis != 0 || zAxis != 0)
+        {
+            playerController.PlayerMove(xAxis, zAxis);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            cameraLock = true;
+
+            if (cameraLock == true)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+                cameraLock = false;
+            }
+        }
+    }
+}
