f5a0f64f9d89355cfad9e156a30b7f1792870230
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2016-11-19 01:03:47 -0500

    Continued working on ObjectInteractions.
    Also deleted NewBehaviorScript that was just randomly lying around in the audio folder

--- Assets/Scripts/InteractionScripts/ObjectsInteractions.cs
+++ Assets/Scripts/InteractionScripts/ObjectsInteractions.cs
@@ -3,15 +3,41 @@ using System.Collections;
 
 public class ObjectsInteractions
 {
+    public string objectName;
+    public int objectValue;
+    public bool objectHeld;
+    public GameObject objectInstance;
+    public Transform objectPosition;
+
+
     //public void HighlightOnHover(){}   <--- Supposedly Ryon has the script that relates to these
-    public void AddObjectToInventory()
+
+    public void AddObjectToInventory(string name , int value, GameObject instance)
     {
         // Place object into your inventory
+        objectName = name;
+        objectValue = value;
+        objectInstance = instance;
     }
 
-    public void HoldObject()
+    public void HoldObject(GameObject instance, Transform position)
     {
         // Instantiate the object in this position
+        objectInstance = instance;
+        objectPosition = position;
+
+        objectHeld = true;
+
+        if (objectHeld == true)
+        {
+            /*
+            if (key is pressed)
+            {
+                LeaveObject();
+            }
+            */
+        }
+
     }
     
     public void LeaveObject()
