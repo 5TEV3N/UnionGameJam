2e2405e9bae42dfc18ee809e4a3df2f233e60375
Phlares <ryon.everett@gmail.com>
2016-11-19 17:37:51 -0500

    Continued work on Generic
    generic script for state and item in progress

---
+++ Assets/Scripts/GenericScripts/BasicItem.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicItem{
+
+	public int identityNumber;
+	public string itemName;
+	public float itemWeight;
+	public bool isHeld;
+	public bool isDropped;
+	//public 
+
+	public BasicItem(){
+		
+	}
+
+}
---
+++ Assets/Scripts/GenericScripts/BasicState.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicState {
+
+	public int stateID;
+	public string stateName;
+	public float timeMarker;
+	//public 
+
+	public BasicState(int p_stateID , string p_stateName){
+		stateID = p_stateID;
+		stateName = p_stateName;
+	}
+	public float MarkTime(){
+		timeMarker = Time.time;
+		return timeMarker;
+	}
+
+}
