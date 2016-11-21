6703f1deddb5a9bf61df2bc98f4186949ad4164a
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2016-11-20 19:25:01 -0500

    Continued Working on Audio and TestScene

--- Assets/Scripts/Audio/Audio.cs
+++ Assets/Scripts/Audio/Audio.cs
@@ -1,35 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using System.Collections.Generic;
 public class Audio
 {
-    public void GetAudio()
-    {
+    public List<AudioSource> gameAudioList = new List<AudioSource>();
+    public float audioVolume = 0.5f;
+    public float audioFadeValue = 0;
 
+    public void GetAudio(AudioSource audio)
+    {
+        // put audio into list
     }
 
     public void PlayAudio()
     {
-        
+        // audio.Play();
     }
 
     public void FadeAudio()
     {
-
+        // Mathf.Lerp (audio, audioFadeValue, time.deltaTime);
     }
 
     public void StopAudio()
     {
-
+        //  audio.Stop();
     }
 
     public void MuteAudio()
     {
-
-    }
-
-    public void SetVolume()
-    {
-
+        //  audio.Mute = true;
     }
 }
