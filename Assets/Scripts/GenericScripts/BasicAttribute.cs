bf4416a9e469b95d4721923fe6e587b0241f0a60
Phlares <ryon.everett@gmail.com>
2016-11-22 01:29:32 -0500

    Added Generic Attribute Script
    Added generic attribute scrip that can get and check values between 0 and 1, where 0 is min and 1 and max

--- Assets/Scripts/GenericScripts/BasicAttribute.cs
+++ Assets/Scripts/GenericScripts/BasicAttribute.cs
@@ -1,15 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BasicAttribute : MonoBehaviour {
+public class BasicAttribute {
 
-	// Use this for initialization
-	void Start () {
-	
+	public float baseAttributeMin = 0f; // this will be our attribute, set it to a value between 0 and 1;
+	public float baseAttributeMax = 1f;
+	public float baseAttributeCurrent;
+	public float baseAttributeAdjustAmount; // how much you will adjust the attribute by;
+
+
+	public BasicAttribute(){
+		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void AdjustAttribute(string command,float adjustAmount = 0){
+		adjustAmount = Mathf.Clamp01 (adjustAmount);
+		switch (command) {
+		case "AdjustToNumber":
+			baseAttributeCurrent = adjustAmount;
+			break;
+		case "AddAmount":
+			baseAttributeCurrent = Mathf.Clamp01 (baseAttributeCurrent += adjustAmount);
+			break;
+		case "SubtractAmount":
+			baseAttributeCurrent = Mathf.Clamp01 (baseAttributeCurrent -= adjustAmount);
+			break;
+		case "MultiplyByAmount":
+			baseAttributeCurrent = Mathf.Clamp01 (baseAttributeCurrent * adjustAmount);
+			break;
+		case "DivideAmount":
+			baseAttributeCurrent = Mathf.Clamp01 (baseAttributeCurrent / adjustAmount);
+			break;
+		case "SetToMax":
+			baseAttributeCurrent = baseAttributeMax;
+			break;
+		case "SetToMin":
+			baseAttributeCurrent = baseAttributeMin;
+			break;
+		default:
+			break;
+
+		}
+		
+	}
+
+	//Check Value Functions
+	public float GetRatio(){
+		float ratio = Mathf.Clamp01 (baseAttributeCurrent / baseAttributeMax);
+		return ratio;
+	}
+	public float GetPercentage(){
+		float percentage = GetRatio () * 100f;
+		return percentage;
+	}
+	public float GetRatioAsRange(float maxValue){
+		float ratioAsRange = GetRatio() * maxValue;
+		return ratioAsRange;
 	}
 }
