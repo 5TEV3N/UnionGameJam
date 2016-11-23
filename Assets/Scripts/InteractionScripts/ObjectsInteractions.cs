c1f53101602e85af2e7fb4077e54a87173b00816
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2016-11-23 01:47:48 -0500

    Continued working on ObjectInteraction

--- Assets/Scripts/InteractionScripts/ObjectsInteractions.cs
+++ Assets/Scripts/InteractionScripts/ObjectsInteractions.cs
@@ -3,47 +3,67 @@ using System.Collections;
 
 public class ObjectsInteractions
 {
-    public string objectName;
-    public int objectValue;
-    public bool objectHeld;
     public GameObject objectInstance;
     public Transform objectPosition;
+    public bool isHighlighted;
+
+    private Color orginalColor;
+    private Renderer interactableObjRender;
+    private GameObject interactableObj;
 
     private BasicItem gameItem = new BasicItem();
 
-    //public void HighlightOnHover(){}   <--- Supposedly Ryon has the script that relates to these
+    public void HighlightOnHover(GameObject p_thisGameObject, Renderer p_interactableObjRender, Color p_objOriginalColor)
+    {
+        orginalColor = p_objOriginalColor;
+        interactableObjRender = p_interactableObjRender;
+        interactableObj = p_thisGameObject;
+
+        if (isHighlighted == true)
+
+        {
+            interactableObjRender = interactableObj.GetComponent<Renderer>();
+            orginalColor = interactableObjRender.material.color;
+            interactableObjRender.material.color = orginalColor + new Color32(200, 200, 200, 1);
+
+        }
+        else
+        {
+            interactableObjRender.material.color = orginalColor;
+        }
+    }
+
 
     public void AddObjectToInventory(string p_name , int p_value, float p_weight)
     {
-        // Place object into your inventory
         gameItem.itemName = p_name;
         gameItem.identityNumber = p_value;
         gameItem.itemWeight = p_weight;
-
+        // Place object into your inventory
     }
 
-    public void HoldObject(GameObject instance, Transform position)
+    public void HoldObject(GameObject p_instance, Transform p_position)
     {
         // Instantiate the object in this position
-        objectInstance = instance;
-        objectPosition = position;
+        objectInstance = p_instance;
+        objectPosition = p_position;
+        gameItem.isHeld = true;
 
-        objectHeld = true;
-
-        if (objectHeld == true)
+        if (gameItem.isHeld == true)
         {
-            /*
             if (Input.GetKeyDown(KeyCode.Q))
             {
-                LeaveObject();
+                gameItem.isHeld = false;
+
+                LeaveObject(p_instance);
             }
-            */
         }
 
     }
     
-    public void LeaveObject()
+    public void LeaveObject(GameObject p_droppedInstance)
     {
         //if holding an object is happening, you may drop that object
+        gameItem.isDropped = true;
     }
 }
