33045af43f36a9927fee4991c106b957143e2ead
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2016-11-24 21:50:23 -0500

    Added Object Script

---
+++ Assets/Scripts/Objects/Objects.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Objects
+{
+    private BasicItem objectVal = new BasicItem();
+
+    public void SetObjectState()
+    {
+
+    }
+    
+    public void GetObjectState()
+    {
+
+    }
+
+    public void DestroyGameObject()
+    {
+
+    }
+
+    public void InstantiateAtLocation()
+    {
+
+    }
+}
