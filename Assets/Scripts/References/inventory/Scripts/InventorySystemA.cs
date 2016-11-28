e0d521cd543f5df2097a6f943ba614d58313f0b6
Phlares <ryon.everett@gmail.com>
2016-11-27 21:43:49 -0500

    Added inventory reference to main script, not yet integrated
    integration in progress

---
+++ Assets/Scripts/References/inventory/Scripts/InventorySystemA.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+
+
+public class InventorySystemA : MonoBehaviour {
+
+   
+
+    GameObject inventoryPanel;
+    GameObject slotPanel;
+    ItemDatabase database;
+    public  GameObject inventorySlot;
+    public GameObject inventoryItem;
+
+    private int slotAmount;
+    public List<Item> items = new List<Item>();
+    public List<GameObject> slots = new List<GameObject>();
+
+    void Start()
+    {
+        database = GetComponent<ItemDatabase>();
+        slotAmount = 10;
+        inventoryPanel = GameObject.Find("Inventory Panel");
+        slotPanel = inventoryPanel.transform.FindChild("Slot Panel").gameObject;
+
+        //Debug.Log(slotPanel.name + " is this different from" + inventoryPanel.transform.FindChild("Slot Panel").gameObject.name);
+        for(int i = 0; i < slotAmount; i++)
+        {
+            items.Add(new Item(0, "bon",0, "equip"));
+            slots.Add(Instantiate(inventorySlot));
+            slots[i].transform.SetParent(slotPanel.transform);
+        }
+
+        AddItem(0);
+        AddItem(1);
+    }
+
+    public void AddItem (int id)
+    {
+          Item itemToAdd = database.FetchItemByID(id);
+        for (int i = 0; i < items.Count; i++)
+        {
+            if (items[i].ID == -1)
+            {
+                items[i] = itemToAdd;
+                GameObject itemObj = Instantiate(inventoryItem);
+                itemObj.transform.SetParent(slots[i].transform);
+                itemObj.transform.position = Vector2.zero;
+                itemObj.GetComponent<Image>().sprite = itemToAdd.Sprite;
+                itemObj.name = itemToAdd.Title;
+                
+                break;
+
+                // https://www.youtube.com/watch?v=5itb0TryrGQ  time: 1:00 [Drag and Drop]
+            }
+        }
+    }
+}
