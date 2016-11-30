930a5c9ad73e06c8a87d401bffd6f41101a6426e
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2016-11-30 02:25:35 -0500

    Continued working on Objects and tweaked the revision main abit
    Also, added a small reminder to fix the raycasting situation

--- Assets/Scripts/Objects/Objects.cs
+++ Assets/Scripts/Objects/Objects.cs
@@ -6,37 +6,11 @@ public class Objects : MonoBehaviour
     //made these public so that the inventorySystem can use them
     public BasicItem objectVal = new BasicItem();
     public State objectState = new State();
-    public Spawner spawn = new Spawner();
-
-	//PRototype START
-	//public PrototypeEngine prototyper;
-
-	//void Awake(){
-	//	prototyper = GameObject.Find("GameMaster").GetComponent<PrototypeEngine>();
-	//}
-
-
-	//void OnTriggerEnter(Collider other){
-	//	if (other.tag == "Player") {
-	//		prototyper.DoNextThing(prototyper.startObjectiveText1,prototyper.starOldManText2);
-	//	}
-		
-	//}
-
-	//Prototype END
-    public void SetObjectState()
-    {
-
-    }
-    
-    public void GetObjectState()
-    {
-
-    }
+    private Spawner spawn = new Spawner();
 
     public void DestroyGameObject(GameObject p_GameObjectInstance)
     {
-
+        Destroy(p_GameObjectInstance);
     }
 
     public void InstantiateAtLocation(GameObject p_GameObjectInstance, Transform p_SpawnPoint)
@@ -44,3 +18,20 @@ public class Objects : MonoBehaviour
         spawn.SpawnObjectAtSpot(p_SpawnPoint, p_GameObjectInstance);
     }
 }
+
+//PRototype START
+//public PrototypeEngine prototyper;
+
+//void Awake(){
+//	prototyper = GameObject.Find("GameMaster").GetComponent<PrototypeEngine>();
+//}
+
+
+//void OnTriggerEnter(Collider other){
+//	if (other.tag == "Player") {
+//		prototyper.DoNextThing(prototyper.startObjectiveText1,prototyper.starOldManText2);
+//	}
+
+//}
+
+//Prototype END
