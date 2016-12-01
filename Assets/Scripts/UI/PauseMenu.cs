999abfec8f460e56a207615b5d1d166df35cda70
Unknown <morais_kimberly@yahoo.ca>
2016-11-30 19:39:56 -0500

    Kim: Created Pause UI Menu & Pauses Time

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject[] pauseObjects;
+
+    // Use this for initialization
+    void Start()
+    {
+        Time.timeScale = 1;
+        hidePaused();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //uses the p button to pause and unpause the game
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+                showPaused();
+            }
+            else if (Time.timeScale == 0)
+            {
+                Time.timeScale = 1;
+                hidePaused();
+            }
+        }
+    }
+
+    //Reloads the Level
+    public void Reload()
+    {
+        //Application.LoadLevel(Application.loadedLevel);
+        SceneManager.LoadScene(0);
+    }
+
+    //controls the pausing of the scene
+    public void pauseControl()
+    {
+        if (Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+            showPaused();
+        }
+        else if (Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+            hidePaused();
+        }
+    }
+
+    //shows objects with ShowOnPause tag
+
+    public void showPaused()
+    {
+        foreach (GameObject g in pauseObjects)
+        {
+            g.SetActive(true);
+        }
+    }
+
+    //hides objects with ShowOnPause tag
+    public void hidePaused()
+    {
+        foreach (GameObject g in pauseObjects)
+        {
+            g.SetActive(false);
+        }
+    }
+
+    //loads inputted level
+    public void LoadLevel()
+    {
+       SceneManager.LoadScene(0); // wait for ryon to do the save/load functions so that you can refference this - Steven
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void SaveLevel()
+    {
+        //waiting for ryon saving script - Kim
+    }
+}
