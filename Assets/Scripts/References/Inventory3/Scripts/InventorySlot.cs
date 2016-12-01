9809093691e18bb1cdaf7f7ec2c3786583152078
Phlares <ryon.everett@gmail.com>
2016-11-30 19:46:53 -0500

    Pushing Inventory3
    full inventory

---
+++ Assets/Scripts/References/Inventory3/Scripts/InventorySlot.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+
+public class InventorySlot : MonoBehaviour, IDropHandler {
+
+    public int id;
+    private InventorySystem inv;
+
+    void Start()
+    {
+        inv = GameObject.Find("Inventory").GetComponent<InventorySystem>();
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        ItemData droppedItem = eventData.pointerDrag.GetComponent<ItemData>();
+        if (inv.items[id].ID == -1)
+        {
+            inv.items[droppedItem.slotLocation] = new Item();
+            inv.items[id] = droppedItem.item;
+            droppedItem.slotLocation = id;
+        }
+        else if(droppedItem.slotLocation != id)
+        {
+            Transform item = this.transform.GetChild(0);
+            item.GetComponent<ItemData>().slotLocation = droppedItem.slotLocation;
+            item.transform.SetParent(inv.slots[droppedItem.slotLocation].transform);
+            item.transform.position = inv.slots[droppedItem.slotLocation].transform.position;
+
+            droppedItem.slotLocation = id;
+            droppedItem.transform.SetParent(this.transform);
+            droppedItem.transform.position = this.transform.position;
+
+            inv.items[droppedItem.slotLocation] = item.GetComponent<ItemData>().item;
+            inv.items[id] = droppedItem.item;
+        }
+    }
+
+
+
+}
---
+++ Assets/Scripts/References/Inventory3/Scripts/InventorySystem.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+
+
+public class InventorySystem : MonoBehaviour {
+
+   
+
+    GameObject inventoryPanel;
+    GameObject slotPanel;
+    ItemDatabase database;
+    public  GameObject inventorySlot;
+    public GameObject inventoryItem;
+
+    private int slotAmount;
+    public List<Item> items = new List<Item>();
+    public List<GameObject> slots = new List<GameObject>();
+
+    void Start()
+    {
+        database = GetComponent<ItemDatabase>();
+        slotAmount = 10;
+        inventoryPanel = GameObject.Find("Inventory Panel");
+        slotPanel = inventoryPanel.transform.FindChild("Slot Panel").gameObject;
+        for(int i = 0; i < slotAmount; i++)
+        {
+            items.Add(new Item());
+            slots.Add(Instantiate(inventorySlot));
+            slots[i].GetComponent<InventorySlot>().id = i;
+            slots[i].transform.SetParent(slotPanel.transform);
+        }
+
+        AddItem(0);
+        AddItem(1);
+    }
+
+    public void AddItem (int id)
+    {
+          Item itemToAdd = database.FetchItemByID(id);
+        for (int i = 0; i < items.Count; i++)
+        {
+            if (items[i].ID == -1)
+            {
+                items[i] = itemToAdd;
+                GameObject itemObj = Instantiate(inventoryItem);
+                itemObj.GetComponent<ItemData>().item = itemToAdd; // Connects with new script <ItemData>
+                itemObj.GetComponent<ItemData>().slotLocation = i;
+                itemObj.transform.SetParent(slots[i].transform);
+                itemObj.transform.position = Vector2.zero;
+                itemObj.GetComponent<Image>().sprite = itemToAdd.Sprite;
+                itemObj.name = itemToAdd.Title;
+                
+                break;
+
+                // https://www.youtube.com/watch?v=5itb0TryrGQ  time: 1:00 [Drag and Drop]
+            }
+        }
+    }
+}
