d73e80f763b4544ed790d82467af5c5d0ac36778
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2016-12-06 01:32:11 -0500

    Added the scenes into the scene builder

---
+++ Assets/Scripts/UI/TitleScreen.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class TitleScreen : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
