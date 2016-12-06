bec367844b81ed22232f4ffc98cafac80b6dc8fd
Phlares <ryon.everett@gmail.com>
2016-12-06 00:41:14 -0500

    Added two scripts for managing trigger zones
    Booyah

---
+++ Assets/Scripts/Environment/EnvironmentTextAfterTime.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnvironmentTextAfterTime : MonoBehaviour {
+
+	public NarrativeEngine narrativeEngine;
+	public bool onTriggerEnter;
+	public bool onTriggerExit;
+	public bool onTriggerStay;
+	public int narrativeLayer;
+	public string narrativeKey;
+	public bool t4Layerf4Key;
+
+	void Awake(){
+		
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (onTriggerEnter == true) {
+		
+		}
+	}
+	void OnTriggerStay(Collider other){
+		if (onTriggerStay == true) {
+
+		}
+	}
+	void OnTriggerExity(Collider other){
+		if (onTriggerExit == true) {
+
+		}
+	}
+}
